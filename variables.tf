6056d1763f8946a8b59e849e1fe5f4c8b3a267e2
nscharan1 <50562264+nscharan1@users.noreply.github.com>
2020-05-18 12:54:46 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  description = "CENTOS AMI ID"
+  default     = "ami-0affd4508a5d2481b"
+}
+
+
+variable "instance_id" {
+  description = "INSTANCE TYPE"
+  default     = "t2.micro"
+}
+
+variable "key_id" {
+  description = "keyname"
+  default     = "pri"
+
+}
